b6435477e3c2a356cd159ceef70c6b6dee10776e
TGdefender247 <dm.v9@outlook.com>
2025-07-07 21:17:21 +0300

    secure-decrypt.js

--- secure-decrypt.js
+++ secure-decrypt.js
@@ -1,3 +1,13 @@
-
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js');
-self.onmessage=function(e){try{var k=CryptoJS.enc.Utf8.parse("MySecretKey12345"),iv=CryptoJS.enc.Utf8.parse("MySecretIV123456"),d=CryptoJS.AES.decrypt(e.data,k,{iv:iv,mode:CryptoJS.mode.CBC,padding:CryptoJS.pad.Pkcs7}).toString(CryptoJS.enc.Utf8);postMessage(d);}catch(o){}};
+self.onmessage = function(e){
+  try {
+    var k = CryptoJS.enc.Utf8.parse("MySecretKey12345"),
+        iv = CryptoJS.enc.Utf8.parse("MySecretIV123456"),
+        d = CryptoJS.AES.decrypt(e.data, k, {
+          iv: iv,
+          mode: CryptoJS.mode.CBC,
+          padding: CryptoJS.pad.Pkcs7
+        }).toString(CryptoJS.enc.Utf8);
+    postMessage(d);
+  } catch(o){}
+};
