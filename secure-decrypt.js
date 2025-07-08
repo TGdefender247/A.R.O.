d5136865552f56436e58cbb66803da247e20c719
TGdefender247 <dm.v9@outlook.com>
2025-07-08 18:38:53 +0300

    secure-decrypt.js

--- secure-decrypt.js
+++ secure-decrypt.js
@@ -4,14 +4,14 @@ onmessage = function(e) {
   const { encrypted, keyHex, ivHex } = e.data;
   try {
     const key = CryptoJS.enc.Hex.parse(keyHex);
-    const iv = CryptoJS.enc.Hex.parse(ivHex);
+    const iv  = CryptoJS.enc.Hex.parse(ivHex);
     const decrypted = CryptoJS.AES.decrypt(encrypted, key, {
       iv: iv,
       mode: CryptoJS.mode.CBC,
       padding: CryptoJS.pad.Pkcs7
     }).toString(CryptoJS.enc.Utf8);
-    postMessage({ decrypted });
+    postMessage(decrypted);
   } catch (err) {
-    postMessage({ decrypted: null });
+    postMessage("error");
   }
-};
+}
